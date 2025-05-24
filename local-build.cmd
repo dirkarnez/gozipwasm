4718a0218771404483b0cf140e2340c89a0c668f
dirkarnez <smalldirkalex@gmail.com>
2025-05-24 20:29:27 +0800

    - wip

--- local-build.cmd
+++ local-build.cmd
@@ -1,11 +1,25 @@
-SET GOOS=windows
-SET GOARCH=amd64
+@echo off
+
+set GOPATH=^
+%USERPROFILE%\Downloads\gopath;
+
+set GOROOT=^
+%USERPROFILE%\Downloads\go1.23.0.windows-amd64\go
+
+set PATH=^
+%GOROOT%\bin;
+
+echo %PATH%
+
+set GOOS=windows
+set GOARCH=amd64
+go get github.com/yeka/zip
 go build
 
-SET GOOS=js
-SET GOARCH=wasm
+@REM SET GOOS=js
+@REM SET GOARCH=wasm
 
-go build -o build-webassembly\main.wasm  .\webassembly\main_js.go
+@REM go build -o build-webassembly\main.wasm  .\webassembly\main_js.go
 
-set GOROOT=%USERPROFILE%\Downloads\go
-copy "%GOROOT%\misc\wasm\wasm_exec.js" build-webassembly\
+@REM set GOROOT=%USERPROFILE%\Downloads\go
+@REM copy "%GOROOT%\misc\wasm\wasm_exec.js" build-webassembly\
--- unzip/unzip.go
+++ unzip/unzip.go
@@ -3,7 +3,38 @@ package unzip
 import (
 	"archive/zip"
 	"bytes"
+
+	yekaZip "github.com/yeka/zip"
 )
+
+// f.SetPassword("12345")
+func UnzipSplitFilesWithPassword(byteArray []byte, password string) ([]*yekaZip.File, error) {
+	r, err := yekaZip.NewReader(bytes.NewReader(byteArray), int64(len(byteArray)))
+	if err != nil {
+		return nil, err
+	}
+
+	return r.File, nil
+
+	// for _, f := range r.File {
+	// 	if f.IsEncrypted() {
+	// 		f.SetPassword("12345")
+	// 	}
+
+	// 	r, err := f.Open()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+
+	// 	buf, err := io.ReadAll(r)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	defer r.Close()
+
+	// 	fmt.Printf("Size of %v: %v byte(s)\n", f.Name, len(buf))
+	// }
+}
 
 func UnzipSplitFiles(byteArray []byte) ([]*zip.File, error) {
 	// Create a new file to write the concatenated content
